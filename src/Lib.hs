3f790524b2f35a6aa437f2ef2180114e3e6097eb
vektordev <viktor_nikolas.pfanschilling@tu-darmstadt.de>
2021-09-01 08:40:24 +0200

    fixed typing issues

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Lib
     ( someFunc
@@ -16,17 +17,79 @@ import Data.List
 import Control.Monad.Random
 import Control.Monad.Except
 import Control.Monad.Reader
+import Data.Reflection (Reifies)
 import Numeric.AD.Internal.Reverse (Reverse)
+import Numeric.AD.Internal.Reverse (Tape)
 
-gradientStep thetas samples = zipWith (+) thetas $ map (0.0001 *) gradient
-  where gradient = [sum (grad (\th -> probability th datum) thetas) | datum <- samples]
+--assumption about grad: Reverse s a is Num if a is Num.
+--Thererfore, grad :: (Traversable f, Num a) => (forall s. Reifies s Tape => f (Reverse s a) -> Reverse s a) -> f a -> f a
+-- essentially translates to grad :: Traversable f, Num a) => (f (Reverse s a) -> Reverse s a) -> f a -> f a
+-- or more simply: grad takes any function :: Traversable f, Num a => f a -> a;
+-- but then we have to sort out the type of the hypothesis (a)
 
-probability :: (Num a) => [a] -> t -> a
-probability = undefined
+--someFunc = print "Hello world"
 
-pr_ :: (Num a, Num b) => a -> Env b -> [a]
+--testExpr :: Num a => Expr a
+testExpr = IfThenElse
+  (GreaterThan Uniform (ThetaI 0))
+  (Constant (VBool True))
+  (Constant (VBool False))
 
-{--
+someFunc :: IO()
+someFunc =
+  let env = [("main", testExpr)] :: Env Float
+  in testRun env
+  
+
+--gradientStep thetas samples = zipWith (+) thetas $ map (0.0001 *) gradient
+--  where gradient = [sum (grad (\th -> probability th datum) thetas) | datum <- samples]
+
+--lambdathing :: forall s a2. (Num a2, Reifies s Tape) => 
+lambdathing :: Num a => Env a -> Expr a -> Value a -> [a] -> a
+lambdathing env expr sample theta = unwrapP $ probability env theta expr sample
+
+something :: Num a => Env a -> Expr a -> Value a -> [a] -> [a]
+something env expr sample = grad (\(theta) -> unwrapP $ probability (autoEnv env) theta (autoExpr expr) (autoVal sample))
+
+myGradientAscent 0 _ _ _ _ = []
+myGradientAscent n env thetas expr vals =
+  (thetas, loss) : myGradientAscent (n-1) env new expr vals
+    where
+      --[gradient] = grad (\[th] -> sum [log $ probabilityFlip th datum | datum <- samples]) [hypothesis]
+      (loss, new) = optimizeStep env expr vals thetas
+
+--TODO: can we fix this to use the Foldable theta?
+optimizeStep :: (Fractional a, Num a) => Env a -> Expr a -> [Value a] -> [a] -> (a, [a])
+optimizeStep env expr samples thetas = (loss, zipWith (+) thetas $ map (*0.0001) gradient)
+  where
+    -- does it make a difference if we do sum(gradients) or if we do gradient(sums)?
+    -- TODO: Is it correct to apply map-sum, or do we flatten the wrong dimension here?
+    --grad_loss :: [(loss :: a, grad :: [a])]
+    grad_loss = [grad' (\(theta) -> unwrapP $ probability (autoEnv env) theta (autoExpr expr) (autoVal sample)) thetas | sample <- samples]
+    gradient = map sum $ Data.List.transpose $ map snd grad_loss
+    loss = sum $ map fst grad_loss
+
+autoExpr :: (Num a, Reifies s Tape) => Expr a -> Expr (Reverse s a)
+autoExpr e = fmap auto e
+
+autoEnv :: (Num a, Reifies s Tape) => Env a -> Env (Reverse s a)
+autoEnv xs = map (\(name, expr) -> (name, autoExpr expr)) xs
+
+autoVal :: (Num a, Reifies s Tape) => Value a -> Value (Reverse s a)
+autoVal (VBool x) = VBool x
+autoVal (VFloat y) = VFloat (auto y)
+
+--myGradientAscent :: (Num a, Num b, Num c, Num d) => Int -> Env a -> [b] -> Expr a -> [Value a] -> [([c], d)]
+--myGradientAscent 0 _ _ _ _ = []
+--myGradientAscent env thetas expr vals = (thetas, loss) : myGradientAscent env newHypothesis expr vals
+--  where
+    --[gradient] = grad (\[th] -> sum [log $ probabilityFlip th datum | datum <- samples]) [hypothesis]
+--    loss = sum $ [unwrapP (log $ probability env thetas expr val) | val <- vals]
+---    gradient = [sum (grad (\ th -> log $ unwrapP $ _ env th expr datum) thetas) | datum <- vals]
+--    newHypothesis = zipWith (+) thetas $ map (\el -> 0.0001 * el) gradient
+--pr_ :: (Num a, Num b) => a -> Env b -> [a]
+
+
 data Expr a = IfThenElse (Expr a) (Expr a) (Expr a)
           | GreaterThan (Expr a) (Expr a)
           | ThetaI Int
@@ -39,6 +102,23 @@ data Expr a = IfThenElse (Expr a) (Expr a) (Expr a)
           | Call String
           | LetIn String (Expr a) (Expr a)
           deriving (Show)
+
+instance Functor Expr where
+  fmap = exprMap
+
+exprMap :: (a -> b) -> Expr a -> Expr b
+exprMap f (IfThenElse a b c) = IfThenElse (fmap f a) (fmap f b) (fmap f c)
+exprMap f (GreaterThan a b) = GreaterThan (fmap f a) (fmap f b)
+exprMap f (ThetaI x) = ThetaI x
+exprMap f Uniform = Uniform
+exprMap f (Constant x) = Constant $ fmap f x
+exprMap f (Mult a b) = Mult (fmap f a) (fmap f b)
+exprMap f (Plus a b) = Plus (fmap f a) (fmap f b)
+exprMap f Null = Null
+exprMap f (Cons a b) = Cons (fmap f a) (fmap f b)
+exprMap f (Call x) = Call x
+exprMap f (LetIn x a b) = LetIn x (fmap f a) (fmap f b)
+
 
 data RType = TBool
            | TFloat
@@ -58,6 +138,10 @@ data Value a = VFloat a
            | VBool Bool
            deriving (Show)
 
+instance Functor Value where
+    fmap f (VFloat a) = VFloat $ f a
+    fmap f (VBool a) = VBool a
+
 data Probability a = PDF a
                  | DiscreteProbability a
                  deriving (Show)
@@ -74,6 +158,18 @@ downgrade = max
 upgrade :: PType -> PType -> PType
 upgrade = min
 
+pAnd :: Num a => Probability a -> Probability a -> Probability a
+pAnd (PDF a) (PDF b) = PDF (a*b)
+pAnd (DiscreteProbability a) (DiscreteProbability b) = DiscreteProbability (a*b)
+
+pOr :: Num a => Probability a -> Probability a -> Probability a
+pOr (PDF a) (PDF b) = PDF (a+b)
+pOr (DiscreteProbability a) (DiscreteProbability b) = DiscreteProbability (a+b)
+
+unwrapP :: Num a => Probability a -> a
+unwrapP (PDF x) = x
+unwrapP (DiscreteProbability x) = x
+
 matchR :: Num a => Expr a -> Expr a -> Check a RType
 matchR e1 e2 = do
   e1R <- inferR e1
@@ -89,7 +185,12 @@ inferR (IfThenElse cond left right) = do
   if condR /= TBool
   then throwError $ Mismatch condR TBool
   else matchR left right
-inferR (GreaterThan left right) = matchR left right
+inferR (GreaterThan left right) = do
+  e1R <- inferR left
+  e2R <- inferR right
+  if e1R /= e2R
+  then throwError $ Mismatch e1R e2R
+  else return TBool
 inferR (ThetaI _) = return TFloat
 inferR Uniform = return TFloat
 inferR (Constant (VFloat _)) = return TFloat
@@ -132,7 +233,7 @@ inferP (Cons head tail) = do
   return $ downgrade headP tailP
 inferP (Call name) = undefined --TODO: here there be dragons
 
-generate :: (RandomGen g, Num a, Ord a, Random a) => Env a -> [a] -> Expr a -> Rand g (Value a)
+generate :: (Fractional a, RandomGen g, Num a, Ord a, Random a) => Env a -> [a] -> Expr a -> Rand g (Value a)
 generate env thetas (IfThenElse cond left right) = do
   condV <- generate env thetas cond
   case condV of
@@ -149,14 +250,7 @@ generate env thetas (ThetaI i) = return $ VFloat (thetas !! i)
 generate env thetas Uniform = do
   r <- getRandomR (0.0, 1.0) --uniformR (0.0, 1.0)
   return $ VFloat r
-
-pAnd :: Num a => Probability a -> Probability a -> Probability a
-pAnd (PDF a) (PDF b) = PDF (a*b)
-pAnd (DiscreteProbability a) (DiscreteProbability b) = DiscreteProbability (a*b)
-
-pOr :: Num a => Probability a -> Probability a -> Probability a
-pOr (PDF a) (PDF b) = PDF (a+b)
-pOr (DiscreteProbability a) (DiscreteProbability b) = DiscreteProbability (a+b)
+generate env thetas (Constant x) = return x
 
 probability :: Num a => Env a -> [a] -> Expr a -> Value a -> Probability a
 -- possible problems in the probability math in there:
@@ -166,98 +260,66 @@ probability env thetas (IfThenElse cond left right) val = pOr (pAnd pCond pLeft)
     pLeft = probability env thetas left val
     pRight = probability env thetas right val
 probability env thetas (GreaterThan left right) val = undefined
---}
-{--
-data SPN = Leaf Float Float | Product [SPN] | Sum [(SPN, Float)]  
 
-data NN = NN [NNLayer]
+prettyPrint :: (Num a, Show a) => Env a -> Expr a -> [String]
+prettyPrint env expr = 
+  fstLine : indented
+    where
+      childExprs = recurse expr
+      indented = map indent $ concatMap (prettyPrint env) childExprs :: [String]
+      indent ls = "    " ++ ls
+      rType = runReader (runExceptT (inferR expr)) env
+      pType = runReader (runExceptT (inferP expr)) env
+      fstLine = printFlat expr ++ " [" ++ show rType ++ " | " ++ show pType ++ "]"
 
-data NNLayer = NNLayer [[Float]] Activation
+recurse :: Expr a -> [Expr a]
+recurse (IfThenElse a b c) = [a,b,c]
+recurse (GreaterThan a b) = [a,b]
+recurse (ThetaI _) = []
+recurse (Uniform) = []
+recurse (Constant _) = []
+recurse (Mult a b) = [a,b]
+recurse (Plus a b) = [a,b]
+recurse (Null) = []
+recurse (Cons a b) = [a,b]
+recurse (Call _) = []
+recurse (LetIn _ a b) = [a,b]
 
-data Activation = Sigmoid | ReLU | Rational | CoRational
+printFlat :: Show a => Expr a -> String
+printFlat (IfThenElse _ _ _) = "IfThenElse"
+printFlat (GreaterThan _ _) = "GreaterThan"
+printFlat (ThetaI i) = "Theta_" ++ show i
+printFlat Uniform = "Uniform"
+printFlat (Constant x) = "Constant (" ++ show x ++ ")"
+printFlat (Mult _ _) = "Mult"
+printFlat (Plus _ _) = "Plus"
+printFlat Null = "Null"
+printFlat (Cons _ _) = "Cons"
+printFlat (Call a) = "Call" ++ a
+printFlat (LetIn _ _ _ ) = "LetIn"
 
---runNNLayer :: NNLayer -> Vector -> Vector
---runNNLayer layer inp = undefined
-  
---someFunc :: IO ()
---someFunc = putStrLn "someFunc"
---  where struct = Product [Leaf 0 0.5, Leaf 0.5, 0.5]
-
---mse :: Float -> Float -> Float
-
---imaginaryData :: [([Float], Float)]
-imaginaryData = [([x, y, z], [x*x]) | x <- [1..10], y <- [1..10], z <- [1..10]]
-
---nnLayer ::  -> Vector -> Vector
---nnLayer :: (Foldable r, Additive r, Functor f, Ord b, Num b) => f (r b) -> r b -> f b
-nnLayer matrix input = fmap activation (matrix !* input)
-  where activation = relu
-
---relu :: (Ord p, Num p) => p -> p
-relu preactivation = if preactivation < 0 then 0 else preactivation
-
---imNN :: (Fractional a, Foldable t, Foldable r, Additive t, Additive r, Ord a) => t (r a) -> r a -> t a -> a
-imNN matrix x y = mseVec (nnLayer matrix x) y
-
---mseVec :: (Fractional a, Foldable t, Additive t) => t a -> t a -> a
-mseVec a b = sum se / realToFrac(length se)
-  where se = fmap (\el -> el * el) (a ^-^ b)
-
---mse :: Num a => a -> a -> a
-mse a b = (a-b) * (a-b)
-
-
---initMatrix :: [[Float]]
-initMatrix = [[1,2,3],[4,5,6]]
-
-testResult = imNN initMatrix (fst $ head imaginaryData) (snd $ head imaginaryData)
-
-test_nn = grad (\mat -> imNN mat (auto x) (auto y)) initMatrix
-  where
-    x = fst $ head imaginaryData
-    y = snd $ head imaginaryData
--}
-{--
-testRun :: Num a => Env a -> IO ()
+testRun :: (Num a, Fractional a, Ord a, Random a, Show a) => Env a -> IO ()
 testRun env = do
   print env
   let Just main = lookup "main" env
+  putStrLn $ unlines $ prettyPrint env main
+  let resultR = runReader (runExceptT (inferR main)) env
+  print resultR
+  let resultP = runReader (runExceptT (inferP main)) env
+  print resultP
   samples <- mkSamples 1000 env [0.5] main
+  mapM_ print samples
   let thetasRecovered = myGradientAscent 100 env [0.1] main samples
   print (reverse thetasRecovered)
 
-mkSamples :: Num a => Int -> Env a -> [a] -> Expr a -> IO [Value a]
+mkSamples :: (Fractional a, Num a, Ord a, Random a) => Int -> Env a -> [a] -> Expr a -> IO [Value a]
 mkSamples 0 _ _ _ = return []
 mkSamples n env thetas expr = do
   sample <- evalRandIO $ generate env thetas expr
   tail <- mkSamples (n-1) env thetas expr
   return (sample:tail)
 
-unwrapP :: Num a => Probability a -> a
-unwrapP (PDF x) = x
-unwrapP (DiscreteProbability x) = x
-
---myGradientAscent :: (Num a, Num b, Num c, Num d) => Int -> Env a -> [b] -> Expr a -> [Value a] -> [([c], d)]
---myGradientAscent 0 _ _ _ _ = []
-myGradientAscent n env thetas expr vals = (thetas, loss) : myGradientAscent (n-1) env newHypothesis expr vals
-  where
-    --[gradient] = grad (\[th] -> sum [log $ probabilityFlip th datum | datum <- samples]) [hypothesis]
-    loss = sum $ [unwrapP (log $ probability env thetas expr val) | val <- vals]
-    gradient = [sum (grad (\ th -> log $ unwrapP $ _ env th expr datum) thetas) | datum <- vals]
-    newHypothesis = zipWith (+) thetas $ map (\el -> 0.0001 * el) gradient
-
-testing2 :: Num a => p1 -> p2 -> p3 -> [a] -> [a]
-testing2 env expr datum = grad (\ th -> unwrapP $ _ env th expr datum)
-
-testing4 :: Fractional a => [a] -> a
-testing4 th = unwrapP $ probability [] th Null $ VFloat 0.0
-
-testing5 :: [a] -> [a]
-testing5 = grad testing4
-
-testing6 :: [a] -> [a]
-testing6 = grad (log testing4)
--}
+{--
 flip theta alpha = if alpha < theta then True else False
 
 probabilityFlip theta sample = if sample then 1 - theta else theta
@@ -302,3 +364,4 @@ asdf = do
   let sample = Lib.flip theta alpha
   print (test_ad_2 theta sample)
 --  print test_nn
+--}
