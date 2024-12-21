22e2347ec8e82b2bc4525432d336e6141c1f3163
dakri7 <daniel.krimm@stud.tu-darmstadt.de>
2024-12-21 15:02:42 +0100

    Added syntactic sugar in Prelude

--- src/SPLL/Prelude.hs
+++ src/SPLL/Prelude.hs
@@ -1,33 +1,59 @@
 module SPLL.Prelude where
 
 import SPLL.Lang.Lang
-import SPLL.Lang.Types (makeTypeInfo)
-{-
-class (Num b, Show b) => Const a where
-  value :: a -> Value b
-  const :: a -> Expr () b
-  const x = Constant () (value x)
+import SPLL.Lang.Types (makeTypeInfo, GenericValue (..))
 
-instance Const Bool where
-  value bl = VBool bl
+-- Flow control
+ifThenElse :: Expr -> Expr -> Expr -> Expr
+ifThenElse = IfThenElse makeTypeInfo
 
---instance Const Float where
---  const bl = Constant () (VFloat bl)
+injF :: String -> [Expr] -> Expr
+injF = InjF makeTypeInfo
 
-instance Const Int where
-  value i = VInt i
+--Arithmetic
 
-instance (Const a) => Const [a] where
-  value lst = VList (map value lst)
--}
-ite :: Expr -> Expr -> Expr -> Expr
-ite = IfThenElse makeTypeInfo
+(#*#) :: Expr -> Expr -> Expr
+(#*#) = MultF makeTypeInfo
 
-(>=) :: Expr -> Expr -> Expr
-(>=) = GreaterThan makeTypeInfo
+(#+#) :: Expr -> Expr -> Expr
+(#+#) = PlusF makeTypeInfo
 
-theta :: Int -> Expr
-theta = undefined -- ThetaI makeTypeInfo 
+(#<*>#) :: Expr -> Expr -> Expr
+(#<*>#) = MultI makeTypeInfo
+
+(#<+>#) :: Expr -> Expr -> Expr
+(#<+>#) = PlusI makeTypeInfo
+
+neg :: Expr -> Expr
+neg = NegF makeTypeInfo
+
+-- Variables
+
+letIn :: String -> Expr -> Expr -> Expr
+letIn = LetIn makeTypeInfo
+
+var :: String -> Expr
+var = Var makeTypeInfo
+
+constF :: Double -> Expr
+constF = Constant makeTypeInfo . VFloat
+
+constI :: Int -> Expr
+constI = Constant makeTypeInfo . VInt
+
+constB :: Bool -> Expr
+constB = Constant makeTypeInfo . VBool
+
+constL :: [Value] -> Expr
+constL = Constant makeTypeInfo . VList
+
+(#->#) :: String -> Expr -> Expr
+(#->#) = Lambda makeTypeInfo
+
+apply :: Expr -> Expr -> Expr
+apply = Apply makeTypeInfo
+
+-- Distributions
 
 uniform :: Expr
 uniform = Uniform makeTypeInfo
@@ -35,17 +61,43 @@ uniform = Uniform makeTypeInfo
 normal :: Expr
 normal = Normal makeTypeInfo
 
-(*) :: Expr -> Expr -> Expr
-(*) = MultF makeTypeInfo
+-- Parameters
 
-(+) :: Expr -> Expr -> Expr
-(+) = PlusF makeTypeInfo
+theta :: Expr -> Int -> Expr
+theta = ThetaI makeTypeInfo
 
-null :: Expr
-null = Null makeTypeInfo
+subtree :: Expr -> Int -> Expr
+subtree = ThetaI makeTypeInfo
+
+-- Product Types
 
 cons :: Expr -> Expr -> Expr
 cons = Cons makeTypeInfo
 
-(<:>) :: Expr -> Expr -> Expr
-(<:>) = cons
+(#:#) :: Expr -> Expr -> Expr
+(#:#) = cons
+
+nul :: Expr
+nul = Null makeTypeInfo
+
+tuple :: Expr -> Expr -> Expr
+tuple = TCons makeTypeInfo
+
+-- Boolean Algebra
+
+(#>#) :: Expr -> Expr -> Expr
+(#>#) = GreaterThan makeTypeInfo
+
+(#<#) :: Expr -> Expr -> Expr
+(#<#) = LessThan makeTypeInfo
+
+(#&&#) :: Expr -> Expr -> Expr
+(#&&#) = And makeTypeInfo
+
+(#||#) :: Expr -> Expr -> Expr
+(#||#) = Or makeTypeInfo
+
+(#!#) :: Expr -> Expr
+(#!#) = Not makeTypeInfo
+
+
