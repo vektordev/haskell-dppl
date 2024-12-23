eec2e2c61b2f529ce4eafaa3d6db20d0ddf18a80
dakri7 <daniel.krimm@stud.tu-darmstadt.de>
2024-12-23 11:04:19 +0100

    Moved IROptimizer to its own file

--- src/SPLL/IRCompiler.hs
+++ src/SPLL/IRCompiler.hs
@@ -5,6 +5,7 @@ import SPLL.Lang.Lang
 import SPLL.Lang.Types
 import SPLL.Typing.Typing
 import SPLL.Typing.RType
+import SPLL.IROptimizer
 import PredefinedFunctions
 import Control.Monad.Supply
 import SPLL.Typing.PType 
@@ -65,135 +66,6 @@ getGlobalTypeEnv p = funcEnv ++ neuralEnv
 runSupplyVars :: (Monad m) => SupplyT Int m a -> m a
 runSupplyVars codeGen = runSupplyT codeGen (+1) 1
 
-isValue :: IRExpr -> Bool
-isValue (IRConst _) = True
-isValue _ = False
-
-isLambda :: IRExpr -> Bool
-isLambda IRLambda {} = True
-isLambda _ = False
-
-unval :: IRExpr -> IRValue
-unval (IRConst val) = val
-unval _ = error "tried to unval a non-val"
-
---strip all top-level lambdas and collect the bound names.
---unwrapTLLambdas :: Expr t a -> ([Varname], Expr t a)
---unwrapTLLambdas (Lambda _ name subExpr) = (name : innerNames, unwrappedExpr)
---  where (innerNames, unwrappedExpr) = unwrapTLLambdas subExpr
---unwrapTLLambdas expr = ([], expr)
-
-postProcess :: IRExpr -> IRExpr
---postProcess = id
-postProcess = fixedPointIteration optimize
-
-fixedPointIteration :: (Eq a, Show a) => (a -> a) -> a -> a
-fixedPointIteration f x = if fx == x then x else fixedPointIteration f fx
-  where fx = f x
-
-optimize :: IRExpr -> IRExpr
-optimize = irMap (evalAll . applyConstant)
---TODO: We can also optimize index magic, potentially here. i.e. a head tail tail x can be simplified.
---TODO: Unary operators
-
--- | Simplify terms that apply a constant to a lambda expression
--- if we build a lambda expression and immediately apply a constant, replace mentions of the lambda'd variable with the constant.
-applyConstant :: IRExpr -> IRExpr
-applyConstant (IRApply (IRLambda varname inExpr) v@(IRConst _)) = replaceAll (IRVar varname) v inExpr
-applyConstant x = x
-
-evalAll :: IRExpr -> IRExpr
--- Associative Addition
-evalAll (IROp OpPlus leftV (IROp OpPlus rightV1 rightV2)) 
-  | isValue leftV && isValue rightV1 = IROp OpPlus (IRConst (forceOp OpPlus (unval leftV) (unval rightV1))) rightV2   -- a + (b + c) = (a + b) + c
-  | isValue leftV && isValue rightV2 = IROp OpPlus (IRConst (forceOp OpPlus (unval leftV) (unval rightV2))) rightV1   -- a + (b + c) = b + (a + c)
-evalAll (IROp OpPlus (IROp OpPlus leftV1 leftV2) rightV ) 
-  | isValue leftV1 && isValue rightV = IROp OpPlus (IRConst (forceOp OpPlus (unval leftV1) (unval rightV))) leftV2   -- a + (b + c) = (a + b) + c
-  | isValue leftV2 && isValue rightV = IROp OpPlus (IRConst (forceOp OpPlus (unval leftV2) (unval rightV))) leftV1   -- a + (b + c) = b + (a + c)
--- Associative Multiplication
-evalAll (IROp OpMult leftV (IROp OpMult rightV1 rightV2)) 
-  | isValue leftV && isValue rightV1 = IROp OpMult (IRConst (forceOp OpMult (unval leftV) (unval rightV1))) rightV2   -- a * (b * c) = (a * b) * c
-  | isValue leftV && isValue rightV2 = IROp OpMult (IRConst (forceOp OpMult (unval leftV) (unval rightV2))) rightV1   -- a * (b * c) = (a * c) * b
-evalAll (IROp OpMult (IROp OpMult leftV1 leftV2) rightV ) 
-  | isValue leftV1 && isValue rightV = IROp OpMult (IRConst (forceOp OpMult (unval leftV1) (unval rightV))) leftV2   -- a + (b + c) = (a + b) + c
-  | isValue leftV2 && isValue rightV = IROp OpMult (IRConst (forceOp OpMult (unval leftV2) (unval rightV))) leftV1   -- a + (b + c) = b + (a + c)
-evalAll expr@(IROp op leftV rightV)
-  | isValue leftV && isValue rightV = IRConst (forceOp op (unval leftV) (unval rightV))
-  | isValue leftV || isValue rightV = softForceLogic op leftV rightV
-  | otherwise = expr 
-evalAll (IRIf _ left right) | left == right = left
-evalAll x@(IRIf cond left right) =
-  if isValue cond
-    then if unval cond == VBool True
-      then left
-      else right
-    else x
-evalAll x@(IRCons left right) =
-  if isValue left && isValue right
-    then let (VList tl) = unval right in IRConst (VList (unval left : tl))
-    else x
-evalAll (IRHead expr) =
-  if isValue expr
-    then let (VList (_:xs)) = unval expr in IRConst $ VList xs
-    else IRHead expr
-evalAll ex@(IRLetIn name val scope)
-  | isSimple val = replaceAll (IRVar name) val scope
-  | countUses name scope == 1 = replaceAll (IRVar name) val scope
-  | countUses name scope == 0 = scope
-  | otherwise = ex
-evalAll (IRTCons (IRLambda n a) (IRLambda m b)) | n == m = IRLambda n (IRTCons a b)
-evalAll (IRDensity IRNormal (IRConst (VFloat x))) = IRConst (VFloat ((1 / sqrt (2 * pi)) * exp (-0.5 * x * x)))
-evalAll (IRCumulative IRNormal (IRConst (VFloat x))) = IRConst (VFloat ((1/2) * (1 + erf(x/sqrt(2)))))
-evalAll (IRDensity IRUniform (IRConst (VFloat x))) = IRConst (VFloat (if x >= 0 && x <= 1 then 1 else 0))
-evalAll (IRCumulative IRUniform (IRConst (VFloat x))) = IRConst (VFloat (if x < 0 then 0 else if x > 1 then 1 else x))
-evalAll x = x
-  
-countUses :: String -> IRExpr -> Int
-countUses var (IRVar a) | a == var = 1
-countUses var expr = sum (map (countUses var) (getIRSubExprs expr))
-
-isSimple :: IRExpr -> Bool
---isSimple (IRTheta a) = True
-isSimple (IRVar a) = True
-isSimple (IRConst a) = True
-isSimple _ = False
-
-replaceAll :: IRExpr -> IRExpr -> IRExpr -> IRExpr
-replaceAll find replaceWith scope = irMap (replace find replaceWith) scope
-
-replace :: Eq p => p -> p -> p -> p
-replace find replace' expr = if find == expr then replace' else expr
-
-failConversion :: Expr -> IRExpr
-failConversion = error "Cannot convert VClosure"
-
-softForceLogic :: Operand -> IRExpr -> IRExpr -> IRExpr
-softForceLogic OpOr (IRConst (VBool True)) _ = IRConst (VBool True)
-softForceLogic OpOr _ (IRConst (VBool True)) = IRConst (VBool True)
-softForceLogic OpOr (IRConst (VBool False)) right = right
-softForceLogic OpOr left (IRConst (VBool False)) = left
-softForceLogic OpAnd (IRConst (VBool True)) right = right
-softForceLogic OpAnd left (IRConst (VBool True)) = left
-softForceLogic OpAnd (IRConst (VBool False)) _ = IRConst (VBool False)
-softForceLogic OpAnd _ (IRConst (VBool False)) = IRConst (VBool False)
-softForceLogic op left right = IROp op left right     -- Nothing can be done
-
-forceOp :: Operand -> IRValue -> IRValue -> IRValue
-forceOp OpEq x y = VBool (x == y)
-forceOp OpMult (VInt x) (VInt y) = VInt (x*y)
-forceOp OpMult (VFloat x) (VFloat y) = VFloat (x*y)
-forceOp OpPlus (VInt x) (VInt y) = VInt (x+y)
-forceOp OpPlus (VFloat x) (VFloat y) = VFloat (x+y)
-forceOp OpDiv (VInt _) (VInt _) = error "tried to do integer division in forceOp"
-forceOp OpDiv (VFloat x) (VFloat y) = VFloat (x/y)
-forceOp OpSub (VInt x) (VInt y) = VInt (x-y)
-forceOp OpSub (VFloat x) (VFloat y) = VFloat (x-y)
-forceOp OpOr (VBool x) (VBool y) = VBool (x || y)
-forceOp OpGreaterThan (VInt x) (VInt y) = VBool (x > y)
-forceOp OpGreaterThan (VFloat x) (VFloat y) = VBool (x > y)
-forceOp OpLessThan (VInt x) (VInt y) = VBool (x < y)
-forceOp OpLessThan (VFloat x) (VFloat y) = VBool (x < y)
-forceOp OpAnd (VBool x) (VBool y) = VBool (x && y)
 
 mkVariable :: String -> CompilerMonad  Varname
 mkVariable suffix = do
